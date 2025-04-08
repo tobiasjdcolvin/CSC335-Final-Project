d8d987460388ce39e558088b3dcdff0169084a5f
Tobias Colvin <107777392+tobiasjdcolvin@users.noreply.github.com>
2025-04-08 12:04:56 -0700

    Created more functionality for the GUI, collaborated with Anthony

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,5 +1,27 @@
 package controller;
 
-public class Controller {
+import model.Model;
+import view.Observer;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Controller implements ActionListener{
+    private Model model;
+
+    public Controller(Model model) {
+        this.model = new Model();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String command = e.getActionCommand();
+        if(command.equals("submit")) {
+            model.submitUserInput("THE USER INPUTTED!?!!");
+        }
+    }
+
+    public void addObserver(Observer observer) {
+        model.registerObserver(observer);
+    }
 }
