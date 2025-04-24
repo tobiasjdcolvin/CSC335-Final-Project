23204f44b9ca2835a6bb911519b573e028d4a6fd
Tobias Colvin <107777392+tobiasjdcolvin@users.noreply.github.com>
2025-04-24 13:38:41 -0700

    Got LandingModel to 100% code coverage.

---
+++ test/model/LandingModelTest.java
@@ -0,0 +1,50 @@
+package model;
+
+import org.junit.Assert;
+import org.junit.Test;
+import view.LandingView;
+import view.Observer;
+
+import java.awt.*;
+
+public class LandingModelTest {
+    public class myObserver implements Observer{
+        @Override
+        public void newText(String text, Color color) {
+            String myString = "this method doesn't need to do anything right now.";
+        }
+    }
+    myObserver myObserverInstance = new myObserver();
+    String myUsername = "jumpo";
+    LandingView myView = new LandingView(myUsername);
+    LandingModel myModel = new LandingModel(myView, myUsername);
+
+    @Test
+    public void testConstructor() {
+        Assert.assertEquals(myModel.getUsername(), myUsername);
+    }
+
+    @Test
+    public void testPlay() {
+        myModel.play();
+        Assert.assertEquals(myModel.getUsername(), myUsername);
+    }
+
+    @Test
+    public void testLogout() {
+        boolean result = myModel.logout();
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void testLeaderboard() {
+        boolean result = myModel.leaderboard();
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void testRegisterDeregister() {
+        myModel.registerObserver(myObserverInstance);
+        myModel.deregisterObserver(myObserverInstance);
+    }
+}
