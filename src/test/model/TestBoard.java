c40ee150310878ff3f384f73ca1b12353c3029fc
Enigmatic-Meep <144402160+Enigmatic-Meep@users.noreply.github.com>
2025-04-24 16:33:04 -0700

    Update TestBoard.java

--- src/test/model/TestBoard.java
+++ src/test/model/TestBoard.java
@@ -7,33 +7,34 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class TestBoard {
-	private Board board = new Board(new Word("apple"),6);
+private Board board = new Board(new Word("apple"),6);
 
-	@Test
-	public void testGetter() {
-	assertEquals(6, board.getGuessRemaining());
-	        board.toggleDebug();
-	        assertEquals(6, board.getGuessRemaining());
-	}
+@Test
+public void testGetter() {
+assertEquals(6, board.getGuessRemaining());
+        board.toggleDebug();
+        assertEquals(6, board.getGuessRemaining());
+}
 
-	@Test
-	public void testGuess() {
-	Guess[] answer = {Guess.GREEN, Guess.GREEN, Guess.YELLOW, Guess.YELLOW, Guess.NONE};
-	assertEquals(answer, board.checkWord(new Word("aplpm")));
-	        board.toggleDebug();
-	        assertEquals(answer, board.checkWord(new Word("aplpm")));
-	}
-		
-	@Test
-	public void testGuess2() {
-		Guess[] answer = {Guess.GREEN, Guess.GREEN, Guess.YELLOW, Guess.YELLOW, Guess.NONE};
-	        //assertTrue(answer.equals(board.checkWord(new Word("aplpm"))));
-	        for(int i = 0; i < 5; i++) {
-	        	assertEquals(answer[i], board.checkWord(new Word("aplpm"))[i]);
-	        	assertEquals((5-i),board.getGuessRemaining());
-	        }
-	 	assertEquals(1, board.getGuessRemaining());
-	        assertFalse(4 == board.getGuessRemaining());
-	 }
+@Test
+public void testGuess() {
+Guess[] answer = {Guess.GREEN, Guess.GREEN, Guess.YELLOW, Guess.YELLOW, Guess.NONE};
+assertEquals(answer, board.checkWord(new Word("aplpm")));
+        board.toggleDebug();
+        assertEquals(answer, board.checkWord(new Word("aplpm")));
+}
+
+
+@Test
+public void testGuess2() {
+Guess[] answer = {Guess.GREEN, Guess.GREEN, Guess.YELLOW, Guess.YELLOW, Guess.NONE};
+//assertTrue(answer.equals(board.checkWord(new Word("aplpm"))));
+for(int i = 0; i < 5; i++) {
+	assertEquals(answer[i], board.checkWord(new Word("aplpm"))[i]);
+	assertEquals((5-i),board.getGuessRemaining());
+}
+assertEquals(1, board.getGuessRemaining());
+assertFalse(4 == board.getGuessRemaining());
+}
 
 }
