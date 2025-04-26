02c3fb4c459690a062e58c198de739f68aa175c8
Tobias Colvin <107777392+tobiasjdcolvin@users.noreply.github.com>
2025-04-25 17:53:29 -0700

    Cleaned up the codebase, reformatted some code, started the README

--- src/model/Board.java
+++ src/model/Board.java
@@ -2,56 +2,56 @@ package model;
 
 
 public class Board {
-private boolean DEBUG = true;
+    private boolean DEBUG = true;
 
-private final model.Word solution;
-private model.Word[] guess;
-private int guessRemaining;
+    private final model.Word solution;
+    private model.Word[] guess;
+    private int guessRemaining;
 
-public Board(Word solution, int size) {
-this.solution = solution;
-this.guessRemaining = size;
+    public Board(Word solution, int size) {
+        this.solution = solution;
+        this.guessRemaining = size;
 
-if (this.DEBUG == true) {
-System.out.println("SOLUTION: " + solution.toString());
-}
-}
+        if (this.DEBUG == true) {
+            System.out.println("SOLUTION: " + solution.toString());
+        }
+    }
 
-public int getGuessRemaining() {
-return this.guessRemaining;
-}
+    public int getGuessRemaining() {
+        return this.guessRemaining;
+    }
 
-public Guess[] checkWord (Word guess) {
-Guess[] ret = {Guess.NONE, Guess.NONE, Guess.NONE, Guess.NONE, Guess.NONE};
-char[] guessChars = guess.getChars();
-char[] solutionChars = this.solution.getChars();
+    public Guess[] checkWord(Word guess) {
+        Guess[] ret = {Guess.NONE, Guess.NONE, Guess.NONE, Guess.NONE, Guess.NONE};
+        char[] guessChars = guess.getChars();
+        char[] solutionChars = this.solution.getChars();
 
 // Check for exact matches first
-for (int i=0;i<5;i++) {
-if (guessChars[i] == solutionChars[i]) {
-ret[i] = Guess.GREEN;
-solutionChars[i] = '#';
-guessChars[i] = '#';
-}
-}
+        for (int i = 0; i < 5; i++) {
+            if (guessChars[i] == solutionChars[i]) {
+                ret[i] = Guess.GREEN;
+                solutionChars[i] = '#';
+                guessChars[i] = '#';
+            }
+        }
 // Check for yellows
-for (int i=0;i<5;i++) {
-for (int j = 0; j<5; j++) {
-if (solutionChars[j] == guessChars[i] && guessChars[i] != '#') {
-ret[i] = Guess.YELLOW;
-solutionChars[j] = '#';
-break;
-}
-}
-}
-this.guessRemaining--;
-if (DEBUG) {
-for (Guess g : ret) {
-System.out.println(g.name());
-}
-}
-return ret;
-}
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (solutionChars[j] == guessChars[i] && guessChars[i] != '#') {
+                    ret[i] = Guess.YELLOW;
+                    solutionChars[j] = '#';
+                    break;
+                }
+            }
+        }
+        this.guessRemaining--;
+        if (DEBUG) {
+            for (Guess g : ret) {
+                System.out.println(g.name());
+            }
+        }
+        return ret;
+    }
 
     public boolean toggleDebug() {
         this.DEBUG = !this.DEBUG;
